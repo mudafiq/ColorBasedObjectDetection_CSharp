61667b05b6326ac206f33ce0b90a4fcc42406eb6
mudafiq <mudafiq.riyan@gmail.com>
2021-06-23 20:56:13 +0700

    Add project files.

---
+++ ColorBasedObjectDetection/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace ColorBasedObjectDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonGreen = new System.Windows.Forms.Button();
+            this.buttonBlue = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(590, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(13, 454);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(112, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Browse Image";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(131, 455);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.Location = new System.Drawing.Point(609, 95);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(185, 23);
+            this.buttonRed.TabIndex = 3;
+            this.buttonRed.Text = "Red";
+            this.buttonRed.UseVisualStyleBackColor = true;
+            this.buttonRed.Click += new System.EventHandler(this.buttonRed_Click);
+            // 
+            // buttonGreen
+            // 
+            this.buttonGreen.Location = new System.Drawing.Point(609, 137);
+            this.buttonGreen.Name = "buttonGreen";
+            this.buttonGreen.Size = new System.Drawing.Size(185, 23);
+            this.buttonGreen.TabIndex = 4;
+            this.buttonGreen.Text = "Green";
+            this.buttonGreen.UseVisualStyleBackColor = true;
+            this.buttonGreen.Click += new System.EventHandler(this.buttonGreen_Click);
+            // 
+            // buttonBlue
+            // 
+            this.buttonBlue.Location = new System.Drawing.Point(609, 181);
+            this.buttonBlue.Name = "buttonBlue";
+            this.buttonBlue.Size = new System.Drawing.Size(185, 23);
+            this.buttonBlue.TabIndex = 5;
+            this.buttonBlue.Text = "Blue";
+            this.buttonBlue.UseVisualStyleBackColor = true;
+            this.buttonBlue.Click += new System.EventHandler(this.buttonBlue_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(500, 454);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(102, 23);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonBlue);
+            this.Controls.Add(this.buttonGreen);
+            this.Controls.Add(this.buttonRed);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button buttonGreen;
+        private System.Windows.Forms.Button buttonBlue;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ ColorBasedObjectDetection/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ColorBasedObjectDetection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog openFile = new OpenFileDialog();
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            openFile.Title = "Browse Image";
+            openFile.Filter = "JPEG Files|*.jpg|PNG Files|*.png";
+
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFile.FileName;
+                Image image = Image.FromFile(openFile.FileName);
+                pictureBox1.Image = image;
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(openFile.FileName);
+        }
+
+        private void buttonRed_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null) return;
+
+                var img = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+                //img._SmoothGaussian(35);
+
+                Bgr lower = new Bgr(0, 0, 120);
+                Bgr higher = new Bgr(100, 40, 255);
+
+                var mask = img.InRange(lower, higher).Not();
+                img.SetValue(new Bgr(0, 0, 0), mask);
+                pictureBox1.Image = img.AsBitmap();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonGreen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null) return;
+
+                var img = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+                img._SmoothGaussian(5);
+
+                Bgr lower = new Bgr(0, 100, 0);
+                Bgr higher = new Bgr(100, 255, 30);
+
+                var mask = img.InRange(lower, higher).Not();
+                img.SetValue(new Bgr(0, 0, 0), mask);
+                pictureBox1.Image = img.AsBitmap();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonBlue_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null) return;
+
+                var img = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+                //
+                img._SmoothGaussian(5);
+
+                Bgr lower = new Bgr(170, 0, 0);
+                Bgr higher = new Bgr(255, 230, 60);
+
+                var mask = img.InRange(lower, higher).Not();
+                img.SetValue(new Bgr(0, 0, 0), mask);
+                pictureBox1.Image = img.AsBitmap();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
